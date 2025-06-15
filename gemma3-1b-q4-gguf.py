7029ce78cbfd86d1c8761ca3e2d017d7da29f94b
nijigen-plot <quark_hardcore@yahoo.co.jp>
2025-06-15 14:57:27 +0900

    stream outputができるように変更

--- gemma3-1b-q4-gguf.py
+++ gemma3-1b-q4-gguf.py
@@ -7,13 +7,13 @@ from llama_cpp import Llama
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-stream_handler = logging.StreamHandler()
-stream_handler.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-stream_handler.setFormatter(formatter)
-logger.addHandler(stream_handler)
+# stream_handler = logging.StreamHandler()
+# stream_handler.setLevel(logging.INFO)
+# formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# stream_handler.setFormatter(formatter)
+# logger.addHandler(stream_handler)
 
-def main():
+def main(stream: bool = True, max_tokens: int = 512):
     parser = argparse.ArgumentParser(description="第一引数をプロンプトとしてうけとりAIに問い合わせます")
     parser.add_argument("prompt", type=str, help="AIに問い合わせるプロンプト")
     args = parser.parse_args()
@@ -21,7 +21,7 @@ def main():
     messages = [
         {
             "role": "system",
-            "content": [{"type": "text", "text": "あなたは日本語を話すAIアシスタントです。"}]
+            "content": [{"type": "text", "text": f"あなたは日本語を話すAIアシスタントです。"}]
         },
         {
             "role": "user",
@@ -38,11 +38,22 @@ def main():
     start = time.perf_counter()
     resp = llm.create_chat_completion(
         messages=messages,
-        max_tokens=512,
-        stream=False,
+        max_tokens=max_tokens,
+        stream=stream,
         temperature=0.3
     )
-    logger.info(resp["choices"][0]["message"]["content"])
+    if stream:
+        partial_message = ""
+        for msg in resp:
+            message = msg["choices"][0]["delta"]
+            if "content" in message:
+                content = message["content"]
+                print(content, end="")
+                partial_message += content
+        logger.info(f"最終的なメッセージ: {partial_message}")
+    else:
+        print(resp["choices"][0]["message"]["content"])
+        logger.info(f"最終的なメッセージ: {resp['choices'][0]['message']['content']}")
     end = time.perf_counter() - start
     logger.info(f"処理時間: {end:.2f}秒")
 
