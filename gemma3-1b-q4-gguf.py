d1b0dc1507a405ba4797a1c75fcb7091860a0e43
nijigen-plot <quark_hardcore@yahoo.co.jp>
2025-06-15 14:57:56 +0900

    black, isort適用

--- gemma3-1b-q4-gguf.py
+++ gemma3-1b-q4-gguf.py
@@ -13,34 +13,31 @@ logger.setLevel(logging.INFO)
 # stream_handler.setFormatter(formatter)
 # logger.addHandler(stream_handler)
 
+
 def main(stream: bool = True, max_tokens: int = 512):
-    parser = argparse.ArgumentParser(description="第一引数をプロンプトとしてうけとりAIに問い合わせます")
+    parser = argparse.ArgumentParser(
+        description="第一引数をプロンプトとしてうけとりAIに問い合わせます"
+    )
     parser.add_argument("prompt", type=str, help="AIに問い合わせるプロンプト")
     args = parser.parse_args()
     prompt = args.prompt
     messages = [
         {
             "role": "system",
-            "content": [{"type": "text", "text": f"あなたは日本語を話すAIアシスタントです。"}]
-        },
-        {
-            "role": "user",
             "content": [
-                {"type": "text", "text": prompt}
-            ]
-        }
+                {"type": "text", "text": f"あなたは日本語を話すAIアシスタントです。"}
+            ],
+        },
+        {"role": "user", "content": [{"type": "text", "text": prompt}]},
     ]
     llm = Llama(
         model_path="./gemma-3-1b-it-qat-q4_0-gguf/gemma-3-1b-it-q4_0.gguf",
         verbose=False,
         n_ctx=1024,
-        )
+    )
     start = time.perf_counter()
     resp = llm.create_chat_completion(
-        messages=messages,
-        max_tokens=max_tokens,
-        stream=stream,
-        temperature=0.3
+        messages=messages, max_tokens=max_tokens, stream=stream, temperature=0.3
     )
     if stream:
         partial_message = ""
@@ -56,6 +53,7 @@ def main(stream: bool = True, max_tokens: int = 512):
         logger.info(f"最終的なメッセージ: {resp['choices'][0]['message']['content']}")
     end = time.perf_counter() - start
     logger.info(f"処理時間: {end:.2f}秒")
+
 
 # loading shardsはpyファイル（プロセス）実行のたびに入るので、いざ使う時は発生しないように対処する
 if __name__ == "__main__":
