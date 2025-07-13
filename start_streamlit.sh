f65086ce623df0ce70f77617da10448540b62a69
nijigen-plot <quark_hardcore@yahoo.co.jp>
2025-07-13 13:42:19 +0900

    exec end-of-file-fixer

--- start_streamlit.sh
+++ start_streamlit.sh
@@ -3,4 +3,3 @@
 cd /home/quark/Work/personal-knowledge-base
 eval "$(/home/quark/.local/share/reflex/bun/bin/direnv export bash)"
 /home/quark/.local/bin/uv run streamlit run streamlit_app.py --server.port $STREAMLIT_APP_PORT
-
